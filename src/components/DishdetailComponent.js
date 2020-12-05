870194caf6b8f624d9a2fecc9cdb63087c8b3f1b
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-12-05 19:16:32 +0500

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render(){
@@ -120,7 +120,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
 
 
-        function RenderComments({comments, addComment, dishId}){
+        function RenderComments({comments, postComment, dishId}){
             if (comments != null) {
                 const commentList = comments.map((comment) => {
                     return (
@@ -140,7 +140,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                         <ul className="list-unstyled">
                             {commentList}
                         </ul>
-                        <CommentForm dishId={dishId} addComment={addComment} /> 
+                        <CommentForm dishId={dishId} postComment={postComment} /> 
                     </div>
                 )
             }
@@ -187,7 +187,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                 <div className="row">
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id} />     
                 </div>
                 </div>
